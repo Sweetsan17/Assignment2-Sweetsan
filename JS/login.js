be7cb9715c166036d55c10479505d358fc9d9b1d
Sweetsan <coderintheworldhack100@gmail.com>
2026-04-09 18:44:54 +0530

    error and success function  added

--- JS/login.js
+++ JS/login.js
@@ -17,5 +17,17 @@ function setError(element,message){
     const errorElement = inputGroup.querySelector(`.error`);
 
     errorElement.innerText = message;
+    inputGroup.classList.add(`error`);
+    inputGroup.classList.remove(`success`);
+    
+}
+
+function setSuccess(element){
+    const inputGroup = element.parentElement;
+    const errorElement = inputGroup.querySelector(`.error`);
+
+    errorElement.innerText = " ";
+    inputGroup.classList.add(`error`);
+    inputGroup.classList.remove(`success`);
     
 }
